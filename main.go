faa5ac524feff651d7a54f0162aec5ea165a8a08
EWOOWE-MIPRO\Administrator <1394166475@qq.com>
2021-11-25 00:26:47 +0800

    v0.1 release

--- main.go
+++ main.go
@@ -3,11 +3,5 @@ package main
 import "watchgoggy/watchgoggy"
 
 func main() {
-	/*pid, err := watchgoggy.GetPid("/java/jdk1.8.0_221/bin/java -Dopennms.home=/omc/config/opennms -jar /omc/omc-application.jar")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(pid)*/
 	watchgoggy.Monitor()
 }
